dfafcdc988ffac0f4c5b2912db129fe8a4191e60
Jorgegarnelo <jorgegarnelolopez@gmail.com>
2026-03-15 15:02:03 +0100

    chore: configurar environments y gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'http://172.28.245.177:8080/api'
 };
 
 /*
